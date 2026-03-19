f097f82b6ba7081006596667216f1cb413e3b5dc
kwonbx <zosia1811@op.pl>
2026-03-19 19:59:13 +0100

    Add RSA algorithm

---
+++ RSA_DH/RSA.py
@@ -0,0 +1,58 @@
+import math
+import random
+
+def findE(phi):
+    e = random.randint(1, phi - 1)
+    while math.gcd(e, phi) != 1:
+        e = random.randint(1, phi - 1)
+    return e
+
+def intoBlocks(m):
+    blocks = []
+    m = str(m)
+    for i in range(0, len(m), 3):
+        s = m[i:i + 3]
+        blocks.append(int(s))
+    return blocks
+
+def compare(m1, m2):
+    m1 = str(m1)
+    m2 = str(m2)
+    for i in range(0, len(m1)):
+        if m1[i] != m2[i]:
+            print("BŁĄD: Wiadomości nie są identyczne!")
+            return False
+    print("Wiadomości są identyczne!")
+
+if __name__ == '__main__':
+    p = 7433
+    q = 2351
+
+    print("p = " + str(p) + ", q = " + str(q))
+
+    n = p * q
+    print("n = " + str(n))
+
+    phi = (p - 1) * (q - 1)
+    print("phi = " + str(phi))
+
+    e = findE(phi)
+    print("e (klucz publiczny) = " + str(e))
+
+    d = pow(e, -1, phi)
+    print("d (klucz prywatny) = " + str(d))
+
+    m = 64323887627863278391893731789316431689819363819410
+
+    blocksM = intoBlocks(m)
+
+    c = [pow(x, e, n) for x in blocksM]
+
+    print("WIADOMOŚĆ ZASZYFROWANA: " + "".join(map(str, c)))
+
+    m2 = [pow(x, d, n) for x in c]
+    m2 = int("".join(map(str, m2)))
+
+    print("WIADOMOŚĆ JAWNA: " + str(m2))
+
+    compare(m, m2)
